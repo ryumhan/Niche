f87ff21512e2edf8dbbc82f6314d23a90de20bd5
ryumhan <ryum7han@gmail.com>
2022-04-21 23:46:50 +0900

    For practice, Navigation Container is applied And image is applied

---
+++ src/screen/opening/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import {Image} from 'react-native';
+
+const Opening = () => {
+  return <Image source={require('../../../image/emblem.jpeg')} />;
+};
+
+export default Opening;
