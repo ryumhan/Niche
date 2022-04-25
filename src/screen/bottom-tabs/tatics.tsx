99c5965d52d3752981b3ff09d46898554c3b6e6c
ryumhan <ryum7han@gmail.com>
2022-04-26 00:35:16 +0900

    Applied tutorial of draggable component

--- src/screen/bottom-tabs/tatics.tsx
+++ src/screen/bottom-tabs/tatics.tsx
@@ -1,10 +1,26 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Alert} from 'react-native';
+import Draggable from 'react-native-draggable';
 
 const Tatics = () => {
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <Text>Tatics Menu!!</Text>
+      <View>
+        <Draggable
+          x={75}
+          y={100}
+          renderSize={56}
+          renderColor="black"
+          renderText="A"
+          isCircle
+          shouldReverse
+          onShortPressRelease={() => Alert.alert('touched!!')}
+        />
+        <Draggable x={200} y={300} renderColor="red" renderText="B" />
+        <Draggable />
+        <Draggable x={50} y={50} />
+      </View>
     </View>
   );
 };
