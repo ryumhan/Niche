c8901569b1fb0d9e425e24b3fca932028b3f77c7
ryumhan <ryum7han@gmail.com>
2022-04-25 23:58:12 +0900

    Changed Opening Screen

--- App.tsx
+++ App.tsx
@@ -1,36 +1,37 @@
-import React from 'react';
-import {StatusBar, useColorScheme, View, Text, Button} from 'react-native';
+import React, {useEffect} from 'react';
+import {StatusBar, useColorScheme} from 'react-native';
 
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+import SplashScreen from 'react-native-splash-screen';
+
 import Opening from './src/screen/opening';
-
-// import Opening from './src/screen/opening';
-
-function DetailsScreen({navigation}: any) {
-  return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Niche App initial</Text>
-      <Button
-        title="Go to Emblem!!"
-        onPress={() => navigation.navigate('Home')}
-      />
-    </View>
-  );
-}
+import Home from './src/screen/bottom-tabs/home';
 
 const Stack = createNativeStackNavigator();
 
 const App = () => {
   const isDarkMode = useColorScheme() === 'dark';
 
+  useEffect(() => {
+    try {
+      setTimeout(() => {
+        SplashScreen.hide(); /** 추가 **/
+      }, 2000); /** 스플래시 시간 조절 (2초) **/
+    } catch (e) {
+      console.warn('에러발생');
+      console.warn(e);
+    }
+  });
+
   return (
     <>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
       <NavigationContainer>
         <Stack.Navigator>
-          <Stack.Screen name="start" component={DetailsScreen} />
-          <Stack.Screen name="Home" component={Opening} />
+          <Stack.Screen name="intro" component={Opening} />
+          <Stack.Screen name="home" component={Home} />
         </Stack.Navigator>
       </NavigationContainer>
     </>
--- src/screen/opening/index.tsx
+++ src/screen/opening/index.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import {Image} from 'react-native';
 
-const Opening = () => {
+const Opening = ({navigation}: any) => {
+  setTimeout(() => {
+    navigation.navigate('home');
+  }, 500);
   return <Image source={require('../../../image/emblem.jpeg')} />;
 };
 
